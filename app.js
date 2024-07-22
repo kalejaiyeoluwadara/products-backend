e076860938bb3f707a1ed4bda3da1c7fbe80f1de
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-07-22 09:10:38 +0100

    first update

---
+++ app.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+require("express-async-errors");
+//async error
+const express = require("express");
+const app = express();
+const notFound = require("./middleware/not-found");
+const errorMiddleWare = require("./middleware/error-handler");
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
+
+// Middleware
+app.use(express.json());
+// routes
+app.get("/", (req, res) => {
+  res.send(
+    '<h1>Store API</h1><a href="/api/v1/products" >Products route</a>  '
+  );
+});
+
+app.use("/api/v1/products", productsRouter);
+
+// Products route
+app.use(notFound);
+app.use(errorMiddleWare);
+const port = process.env.PORT || 3000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
