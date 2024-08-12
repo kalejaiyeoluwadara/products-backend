0758f89b673590d190c0e20d393579d7d70f5920
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-08-12 12:53:41 +0100

    fixed code

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,7 +1,7 @@
 const recipes = require("../db.json");
 
 const getRecipes = (req, res) => {
-  res.status(200).send(recipes);
+  res.status(200).json({ recipes });
 };
 
 const getSingleRecipe = (req, res) => {
@@ -9,7 +9,7 @@ const getSingleRecipe = (req, res) => {
   const recipe = recipes.recipes.find((r) => r.id === id);
 
   if (recipe) {
-    res.status(200).send(recipe);
+    res.status(200).json({ recipe });
   } else {
     res.status(404).send({ message: "Recipe not found" });
   }
